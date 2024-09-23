aa4c9a90b19a70fa5747d500eae0256ddff91e6f
raiolaser15 <133381744+raiolaser15@users.noreply.github.com>
2024-09-23 10:37:30 -0300

    23/09

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Nutricao-isabele
+# 2Tec-Nutricao
+Projeto base para uso dos alunos do 2 ano do curso técnico em desenvolvimento de sistemas.
---
+++ main.js
@@ -0,0 +1,9 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Lucas Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
