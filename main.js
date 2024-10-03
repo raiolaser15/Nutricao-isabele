96b9c506fccc9a8f5599961879363a42cd3623e7
raiolaser15 <133381744+raiolaser15@users.noreply.github.com>
2024-10-03 08:36:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Lucas Nutrição";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < 5; i++) {
+  console.log(pacientes[i]);
+  
+  var tdPeso = pacientes[i].querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = document.querySelector(".info-altura");
+var tdAltura = pacientes[i].querySelector(".info-altura");
 var altura = tdAltura.textContent;
+
+var tdImc = pacientes[i].querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso < 0 || peso > 600) {
+  tdPeso.textContent = "Peso inválido!";
+  pesoEhValido = false;
+  pacientes[i].classList.add("dados-invalidos");
+}
+
+if (altura < 0 || altura > 2.80) {
+  tdAltura.textContent = "Altura inválida!";
+  alturaEhValida = false;
+  pacientes[i].classList.add("dados-invalidos");
+}
+
+if (pesoEhValido && alturaEhValida) {
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc.toFixed(2);
+}
+}
+
+var paciente = document.querySelector("#primeiro-paciente");
