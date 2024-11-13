dcff6929493630d0b1a3cc99ca8464042805b84b
raiolaser15 <133381744+raiolaser15@users.noreply.github.com>
2024-11-13 08:14:50 -0300

    Update form.js

--- form.js
+++ form.js
@@ -9,7 +9,29 @@ var botaoAdicionar = document.querySelector('#adicionar-paciente');
 botaoAdicionar.addEventListener('click', function(event) {
   event.preventDefault();
   
-  var form = document.querySelector('#form-adiciona');
+      var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
   
   //extraindo os dados digitados no formulario
 var paciente = obtemPacienteDoFormulario(form);
